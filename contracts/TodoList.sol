7357eaec116d20c9d413aa2692c7891829855733
RAKSHA V G <vgraksha26@gmail.com>
2022-02-17 12:51:42 +0530

    list tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity ^0.5.0;
 //declare smart contract
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        //initialize taskCount
+        createTask("First task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }

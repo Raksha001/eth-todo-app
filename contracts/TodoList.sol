666bfc866452b30fadc020e9f4333edac8426962
RAKSHA V G <vgraksha26@gmail.com>
2022-02-17 14:38:50 +0530

    complete tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -13,13 +13,22 @@ contract TodoList {
 
     event TaskCreated(uint256 id, string content, bool completed);
 
+    event TaskCompleted(uint256 id, bool completed);
+
     constructor() public {
-        createTask("First task");
+        createTask("First Task");
     }
 
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
